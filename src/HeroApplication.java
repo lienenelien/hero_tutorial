51d906f1e7fd52e6e6474bb71b16e51d0d44d762
lienenelien <liene.lagzdina25@gmail.com>
2021-07-02 21:35:07 +0300

    Initial commit

--- src/HeroApplication.java
+++ src/HeroApplication.java
@@ -3,7 +3,7 @@ public class HeroApplication {
     public static void main(String[] args) {
 
 
-//  Variable initializaion
+//  Variable initialization
 
         String heroName;
         String realName;
@@ -21,32 +21,31 @@ public class HeroApplication {
         heroCity = "Beijing";
         universe = "Earth Comics";
 
-        heroPower1 = ">>> Fast delivery >>>";
-        heroPower2 = "%%% Super discounts %%%";
-        heroPower3 = "!!! Excellent English skills !!!";
+        heroPower1 = ">>>Fast delivery>>>";
+        heroPower2 = "% Super discounts %";
+        heroPower3 = "!!!Excellent English skills!!!";
         isEvil = false;
         heroSalary = 666.666;
 
-//        Print out Hero information card
+//   Print out Hero information card
         System.out.println("**************************");
         System.out.println("HERO INFORMATION");
         System.out.println("**************************");
 
         System.out.println("Hero name:");
         System.out.println(heroName);
-        System.out.println("Heroes real name -"+ realName );
+        System.out.println("Heroes real name -" + realName);
         System.out.println("This Hero lives in -" + heroCity);
-        System.out.println("Universe Hero resides -" +universe);
+        System.out.println("Universe Hero resides -" + universe);
 
         System.out.println("_______________________________");
         System.out.println("Hero Superpowers");
-        System.out.println("---" + heroPower1);
-        System.out.println("---" + heroPower2);
-        System.out.println("---" + heroPower3);
+        System.out.println("- " + heroPower1);
+        System.out.println("- " + heroPower2);
+        System.out.println("- " + heroPower3);
         System.out.println("_______________________________");
         System.out.println("Is this Hero evil? -" + isEvil);
         System.out.println("_______________________________");
-        System.out.println("Hero wage" + heroSalary + "USD");
+        System.out.println("Heroes wage -" + heroSalary + "USD");
         System.out.println("_______________________________");
-
-
+    } }
